a09a733fed30d42d4616d11617fbf0bbae9283de
Shivaprasad Prabhu <prabhushivaprasad17@gmail.com>
2026-04-03 08:56:37 +0530

    Delete Obj directory

--- Obj/factory_alert_system.d
+++
@@ -1,6 +0,0 @@
-.\obj\factory_alert_system.o: Factory_Alert_System.c
-.\obj\factory_alert_system.o: C:\Keil\ARM\RV31\INC\RTL.h
-.\obj\factory_alert_system.o: C:\Keil\ARM\Inc\Philips\LPC23xx.H
-.\obj\factory_alert_system.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
-.\obj\factory_alert_system.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
-.\obj\factory_alert_system.o: LCD.h
--- Obj/lcd.d
+++
@@ -1,4 +0,0 @@
-.\obj\lcd.o: LCD.c
-.\obj\lcd.o: C:\Keil\ARM\RV31\INC\RTL.h
-.\obj\lcd.o: C:\Keil\ARM\Inc\Philips\LPC23xx.H
-.\obj\lcd.o: LCD.h
